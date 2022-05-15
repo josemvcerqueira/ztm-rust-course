14fea0b122ce8cef8f824b8b2dffcce4d153f9de
josemvcerqueira <jose@ilovemochi.com>
2022-05-15 15:21:28 +0100

    feat: a2

--- src/bin/a2.rs
+++ src/bin/a2.rs
@@ -8,4 +8,14 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+fn display(result: i32) {
+    println!("{:?}", result);
+}
+
+fn main() {
+    display(add(10, 15));
+}
