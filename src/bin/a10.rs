ddfdbf820ee7a6c5f96521f543950faf2e62dddf
josemvcerqueira <jose@ilovemochi.com>
2022-05-19 23:11:09 +0100

    feat: a10 solution

--- src/bin/a10.rs
+++ src/bin/a10.rs
@@ -12,4 +12,16 @@
 // * Use a match expression to determine which message
 //   to print
 
-fn main() {}
+fn handle_is_big(x: bool) {
+    match x {
+        true => println!("its big"),
+        false => println!("its small"),
+    }
+}
+
+fn main() {
+    let num = 99;
+    let is_big = num > 100;
+
+    handle_is_big(is_big);
+}
