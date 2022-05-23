afeab718b81e8b13314bf5202a0a5a57a81fd2b6
josemvcerqueira <jose@ilovemochi.com>
2022-05-24 00:49:18 +0100

    solution to a14

--- src/bin/a14.rs
+++ src/bin/a14.rs
@@ -11,4 +11,42 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+struct Person {
+    color: String,
+    name: String,
+    age: i32,
+}
+
+fn print_name(name: &str) {
+    println!("person name is {:?}", name);
+}
+
+fn print_color(color: &str) {
+    println!("person favorite color is {:?}", color);
+}
+
+fn main() {
+    let people = vec![
+        Person {
+            color: "blue".to_owned(),
+            age: 29,
+            name: "Pedro".to_owned(),
+        },
+        Person {
+            color: "pink".to_owned(),
+            age: 28,
+            name: "Maesto".to_owned(),
+        },
+        Person {
+            color: "black".to_owned(),
+            age: 30,
+            name: "Jose".to_owned(),
+        },
+    ];
+
+    for person in people {
+        print_name(&person.name);
+        print_color(&person.color);
+        println!("person is {:?} years old", person.age);
+    }
+}
