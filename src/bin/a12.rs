ca0d5580e376a1024f22ae784ab6d82380da2223
josemvcerqueira <jose@ilovemochi.com>
2022-05-23 23:58:45 +0100

    solutions for a12 and a13

--- src/bin/a12.rs
+++ src/bin/a12.rs
@@ -10,4 +10,47 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+enum Color {
+    Black,
+    White,
+    Brown,
+}
+
+struct ShippingBox {
+    dimensions: i32,
+    weight: f64,
+    color: Color,
+}
+
+impl ShippingBox {
+    fn black_box() -> Self {
+        Self {
+            dimensions: 64,
+            weight: 11.5,
+            color: Color::Black,
+        }
+    }
+
+    fn print_dimensions(&self) {
+        println!("Box has a dimension of {:?}", self.dimensions);
+    }
+
+    fn print_color(&self) {
+        match self.color {
+            Color::Black => println!("Box is black"),
+            Color::White => println!("Box is white"),
+            Color::Brown => println!("Box is brown"),
+        }
+    }
+
+    fn print_weight(&self) {
+        println!("Box weighs {:?}", self.weight);
+    }
+}
+
+fn main() {
+    let black_box = ShippingBox::black_box();
+    black_box.print_color();
+    black_box.print_weight();
+    black_box.print_dimensions();
+}
--- src/bin/a13.rs
+++ src/bin/a13.rs
@@ -10,4 +10,23 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn print_num(&num: &i32) {
+    match num {
+        30 => println!("thirty"),
+        _ => println!("{:?}", num),
+    }
+}
+
+fn print_len(vec: Vec<i32>) {
+    println!("This vector has a length of {:?}", vec.len());
+}
+
+fn main() {
+    let nums = vec![10, 20, 30, 40];
+
+    for num in &nums {
+        print_num(&num);
+    }
+
+    print_len(nums);
+}
