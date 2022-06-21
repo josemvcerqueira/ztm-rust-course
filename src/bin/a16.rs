b9e06ee34a2d86da012a519630c0715cbc5c9929
josemvcerqueira <jose@ilovemochi.com>
2022-06-21 06:40:57 +0100

    feat: a16 solution

--- src/bin/a16.rs
+++ src/bin/a16.rs
@@ -11,4 +11,21 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-fn main() {}
+struct Assignment {
+    name: String,
+    locker: Option<i32>,
+}
+
+fn main() {
+    let student_assignment = Assignment {
+        name: "jose".to_owned(),
+        locker: Some(32),
+    };
+
+    println!("The student name is {:?}", student_assignment.name);
+
+    match student_assignment.locker {
+        Some(combination) => println!("His locker combination is {:?}", combination),
+        None => print!("This student has no locker"),
+    }
+}
